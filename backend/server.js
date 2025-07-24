fc03827d73d7d5a791252f3b7abe92b187aba506
Ishant Mishra <dev.frontend.03@proton.me>
2025-07-24 20:16:43 +0545

    Python requirements.txt fixed

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,8 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(',').map(origin => origin.trim())
   : [];
 
+console.log(allowedOrigins);
+
 const corsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
